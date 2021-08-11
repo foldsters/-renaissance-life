be629b6712bc8b07fd0f1d996c66b1e69296f9bc
Foldster <foldsters@gmail.com>
2021-08-11 14:15:57 -0400

    Hilt setup and First Injection

--- app/src/main/java/com/steamtechs/renaissancelife/MainActivity.kt
+++ app/src/main/java/com/steamtechs/renaissancelife/MainActivity.kt
@@ -2,10 +2,22 @@ package com.steamtechs.renaissancelife
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.steamtechs.renaissancelife.framework.db.CategoryDao
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    @Inject
+    lateinit var dao: CategoryDao
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        println("DAO HASH")
+        println(dao.hashCode())
+
     }
 }
