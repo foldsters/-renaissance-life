cf8e5b9227400780169a3f5f9260c557271e83cc
Chapingro1 <ieatgrub@gmail.com>
2021-08-12 16:08:56 -0400

    Update Class name after previous reversion

--- core/src/main/java/com/steamtechs/core/domain/businesslogic/CategoryScraper.kt
+++ core/src/main/java/com/steamtechs/core/domain/businesslogic/CategoryScraper.kt
@@ -1,10 +1,10 @@
 package com.steamtechs.core.domain.businesslogic
 
-import com.steamtechs.core.data.DayCatLog
+import com.steamtechs.core.data.DayCategoryLog
 import com.steamtechs.core.domain.Category
 
 object CategoryScraper {
-    fun getCategoryFromDate(dayCatLog: DayCatLog): Iterable<Category> {
+    fun getCategoryFromDate(dayCatLog: DayCategoryLog): Iterable<Category> {
         val localList = dayCatLog.getCategories()
         return localList
     }
