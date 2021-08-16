bdd6072cd2e6fbb960527559aeb59eb30b032ab4
Chapingro1 <ieatgrub@gmail.com>
2021-08-16 16:19:15 -0400

    optimized imports to FRoomCategoryDataSourceTest

--- app/src/test/java/com/steamtechs/renaissancelife/framework/datasources/FRoomCategoryDataSourceTest.kt
+++ app/src/test/java/com/steamtechs/renaissancelife/framework/datasources/FRoomCategoryDataSourceTest.kt
@@ -1,8 +1,7 @@
 package com.steamtechs.renaissancelife.framework.datasources
 
 import org.junit.jupiter.api.Assertions.*
-import org.junit.jupiter.api.DisplayName
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.*
 
 internal class FRoomCategoryDataSourceTest {
 
