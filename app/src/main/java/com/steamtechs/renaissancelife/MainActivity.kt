f9056ddbf81e186be7b48e93e64723360bd15fe8
Chapingro1 <ieatgrub@gmail.com>
2021-08-17 12:43:51 -0400

    ScaffoldLayout Added

--- app/src/main/java/com/steamtechs/renaissancelife/MainActivity.kt
+++ app/src/main/java/com/steamtechs/renaissancelife/MainActivity.kt
@@ -1,12 +1,28 @@
 package com.steamtechs.renaissancelife
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.text.ClickableText
+import androidx.compose.material.FloatingActionButton
+import androidx.compose.material.Icon
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.tooling.preview.Preview
+import com.steamtechs.core.domain.Category
+import com.steamtechs.renaissancelife.ui.CategoryRow
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -18,10 +34,9 @@ class MainActivity : AppCompatActivity() {
     }
 }
 
+
+
 @Preview
 @Composable
 fun MyFirstComposable(){
-    Box{
-        Text(text = "Stuffs Here!")
-    }
 }
