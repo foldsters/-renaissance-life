26132b7a720ad33758a9a67cb1e644ab6afb41dc
Foldster <foldsters@gmail.com>
2021-08-17 19:33:02 -0400

    Login MVP Done

--- app/src/main/java/com/steamtechs/renaissancelife/ui/composables/Toggle.kt
+++ app/src/main/java/com/steamtechs/renaissancelife/ui/composables/Toggle.kt
@@ -1,19 +1,16 @@
 package com.steamtechs.renaissancelife.ui.composables
 
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.material.Button
-import androidx.compose.material.DropdownMenu
-import androidx.compose.material.DropdownMenuItem
-import androidx.compose.material.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.steamtechs.renaissancelife.ui.AppViewModel
 import com.steamtechs.renaissancelife.ui.LiveCategory
